29c0dd86f84785a519d71bc98cc7a4161615c6f4
Darinel Valencia <darinel.valencia@gmail.com>
2020-05-06 11:06:02 -0500

    Cambio de datos config/email.php

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'darinel.valencia@unach.mx'),
+        'address' => env('MAIL_FROM_ADDRESS', 'darinel.valencia@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'BU Soporte'),
     ],
 
